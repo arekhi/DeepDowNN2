69b250cb12625fe612a173dcf8ccc832d136b76f
Ubuntu <ubuntu@ip-172-31-11-63.us-west-1.compute.internal>
2018-06-04 00:49:10 +0000

    First push of definition-to-clue network

--- code/helper_functions.py
+++ code/helper_functions.py
@@ -57,6 +57,62 @@ def choose_word_clue_pairs(NUM_TRAIN, word_clue_pairs_list, word_glove_pairs_dic
 
     return words, indices, clues, num_pairs_added, max_clue_length
 
+def choose_word_clue_pairs_with_dict(NUM_TRAIN, word_clue_pairs_list, word_glove_pairs_dict, word_to_index_dict, word_defn_pairs_dict):
+
+    # Make a new list: for the word-clue pairs whose words appear in the word-glove 
+    #   dict, translate that pair into a pair [emb_word, emb_clue_list], where
+    #   emb_clue_list is the list [emb_clue_word_0, emb_clue_word_1, ...].
+    import re
+    num_pairs_added = 0
+    done_flag = 0
+    words = []
+    indices = []
+    clues = []
+    definitions = []
+    words_present = 0
+    max_clue_length = 0
+    max_defn_length = 0
+    for pair in word_clue_pairs_list:
+        if done_flag:
+            break
+        word = pair[1].lower()
+        if (word in word_glove_pairs_dict) and (word in word_defn_pairs_dict):
+            words_present += 1
+            clue = pair[0].lower()
+            defn = word_defn_pairs_dict[word].lower()
+            for ch in [',', ':', '.', ';', '"', '\'', '!', '?', '$', '%', '(', ')']:
+                clue = clue.replace(ch, '')
+                defn = defn.replace(ch, '')
+            clue = clue.replace('-', ' ')
+            defn = defn.replace('-', ' ')
+            defn = re.sub('<(.*?)>', '', defn)
+#            print(clue)
+            clue = clue.split()
+            defn = defn.split()
+            missing_word_flag = 0
+            for clue_word in clue:
+                if clue_word not in word_glove_pairs_dict:
+                    missing_word_flag = 1
+                    break
+            if not missing_word_flag:
+                for defn_word in defn:
+                    if defn_word not in word_glove_pairs_dict:
+                        missing_word_flag = 1
+                        break
+            if not missing_word_flag:
+                words.append(word)
+                indices.append(word_to_index_dict[word])
+                word_embedding = word_glove_pairs_dict[word]
+                clues.append(clue)
+                if len(clue) > max_clue_length:
+                    max_clue_length = len(clue)
+                definitions.append(defn)
+                if len(defn) > max_defn_length:
+                    max_defn_length = len(defn)
+                num_pairs_added += 1
+                if num_pairs_added >= NUM_TRAIN:
+                    done_flag = 1
+    return words, indices, clues, definitions, num_pairs_added, max_clue_length, max_defn_length
 
 def add_tokens(word_glove_pairs_dict, word_to_index_dict, index_to_word_dict, glove_length, clues, max_clue_length, np):
 
@@ -88,3 +144,49 @@ def add_tokens(word_glove_pairs_dict, word_to_index_dict, index_to_word_dict, gl
         training_clue_indices.append(clue_list)
  
     return word_glove_pairs_dict, word_to_index_dict, index_to_word_dict, training_clue_indices, clues
+
+def add_tokens_with_dict(word_glove_pairs_dict, word_to_index_dict, index_to_word_dict, glove_length, clues, max_clue_length, np, definitions, MAX_DEFN_LEN):
+
+    # Add start, end, and pad tokens to each dictionary
+    start_token = np.random.randn(glove_length,)
+    end_token = np.random.randn(glove_length,)
+    pad_token = np.zeros((glove_length,))
+    word_glove_pairs_dict['<START>'] = start_token
+    word_glove_pairs_dict['<END>'] = end_token
+    word_glove_pairs_dict['<PAD>'] = pad_token
+    word_to_index_dict['<START>'] = len(word_to_index_dict)
+    word_to_index_dict['<END>'] = len(word_to_index_dict)
+    word_to_index_dict['<PAD>'] = len(word_to_index_dict) 
+    index_to_word_dict[word_to_index_dict['<START>']] = '<START>'
+    index_to_word_dict[word_to_index_dict['<END>']] = '<END>'
+    index_to_word_dict[word_to_index_dict['<PAD>']] = '<PAD>'
+    
+    # Add start, end, and (as necessary) pad tokens to each clue
+    training_clue_indices = []
+    for i in range(len(clues)):
+        clue = ['<START>'] + clues[i] + ['<END>']
+        pad_length = max_clue_length + 2 - len(clue)
+        for j in range(pad_length):
+            clue = clue + ['<PAD>']
+        clues[i] = clue
+        clue_list = []
+        for word in clue:
+            clue_list.append(word_to_index_dict[word])
+        training_clue_indices.append(clue_list)
+ 
+    # Clip and pad definitions
+    definition_indices = []
+    for i in range(len(definitions)):
+        defn = definitions[i]
+        if len(defn) > MAX_DEFN_LEN:
+            defn = defn[0:MAX_DEFN_LEN]
+        pad_length = MAX_DEFN_LEN - len(defn)
+        for j in range(pad_length):
+            defn = defn + ['<PAD>']
+        definitions[i] = defn
+        defn_list = []
+        for word in defn:
+            defn_list.append(word_to_index_dict[word])
+        definition_indices.append(defn_list)
+
+    return word_glove_pairs_dict, word_to_index_dict, index_to_word_dict, training_clue_indices, definition_indices, clues, definitions
--- code/store_definitions.py
+++ code/store_definitions.py
@@ -4,38 +4,56 @@ import string
 import pickle
 
 ###############################################################################
-# Store word-defn pairs from open-source dictionary in a csv file 
-word_defn_pairs_list = list()
+# Store word-defn pairs from open-source dictionary in a txt file 
+word_defn_pairs_dict = {}
 
 # Go through the open-source dictionary, letter by letter
 for letter in string.ascii_uppercase:
     print('Working on letter ' + letter + '\n')
     # Open and parse dictionary file for this letter
     gnuCIDE_file = open('../data/dictionary/gcide-0.52/CIDE.' + letter, 'r', errors = 'ignore')
+    content = gnuCIDE_file.readlines()
 
-    # If entry found in a line, store it and its definition
-    for line in gnuCIDE_file:
+#    print(content[0:10])
+#    print(len(content))
+#    print(range(len(content)))
+
+    # If entry found in a line, store it and its definition (ignoring obsolete definitions)
+    for i in range(len(content)):
+#        print(i)
+        line = content[i]
         match_word = re.search(r'<ent>(.*?)</ent>', line)
         if match_word:
-            word = match_word.group(1)
-            line = next(gnuCIDE_file)
-            match_defn = re.search(r'<def>(.*?)</def>', line)
-            if match_defn:
-                defn = match_defn.group(1)
-                word_defn_pairs_list.append([word,defn])
+            word = match_word.group(1).lower()
+            keep_going = 1
+            while keep_going and i < (len(content) - 1):
+                i += 1
+#                print(i)
+                line = content[i]
+                match_obs = re.search(r'Obs.', line)
+                match_defn = re.search(r'<def>(.*?)</def>', line)
+                match_word = re.search(r'<ent>(.*?)</ent>', line)
+#                print(match_obs, match_defn, match_word)
+                if match_word:
+                    keep_going = 0
+                    i -= 1
+                    break
+                if match_obs:
+                    pass
+                if (not match_obs) and match_defn:
+#                    print('Defn found')
+                    defn = match_defn.group(1).lower()
+                    if word not in word_defn_pairs_dict:
+                        word_defn_pairs_dict[word] = defn
+                    keep_going = 0
 
     # Close dictionary file for this letter
     gnuCIDE_file.close()
 
 # Print number of words in dictionary
-print(str(len(word_defn_pairs_list)) + ' word/definition pairs were retrieved and stored.\n')
+print(str(len(word_defn_pairs_dict)) + ' word/definition pairs were retrieved and stored.\n')
 
-# Save word-definition pairs list
+# Save word-definition pairs dict
 with open('../data/word_defn_pairs.txt', 'wb') as fp:
-    pickle.dump(word_defn_pairs_list, fp)
-
-# Print word-definition pairs
-#for row in word_defn_pairs_list:
-#    print(row[0])
-
+    pickle.dump(word_defn_pairs_dict, fp)
 ###############################################################################
